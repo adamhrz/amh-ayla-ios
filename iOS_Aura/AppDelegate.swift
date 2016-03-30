bd489eb14201b9ef3ae60b51b0f07fdb1814c3be
yipei <yipei@aylanetworks.com>
2016-03-29 18:25:24 -0700

    Call core manager's pause/resuem application activity has been changed.

--- iOS_Aura/AppDelegate.swift
+++ iOS_Aura/AppDelegate.swift
@@ -37,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        AylaCoreManager.sharedManager().pause()
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -50,6 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        AylaCoreManager.sharedManager().resume()
     }
 
     func applicationWillTerminate(application: UIApplication) {
