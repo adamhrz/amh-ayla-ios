6049879bf0573558fe086a577c1b528c858fa5ed
Kevin Bella <kevin@aylanetworks.com>
2016-04-12 16:56:57 -0700

    Adds new labels for oemModel, Model, MAC and LAN IP to device details page.

--- iOS_Aura/Device/Presentation/DevicePanelView.swift
+++ iOS_Aura/Device/Presentation/DevicePanelView.swift
@@ -14,14 +14,38 @@ class DevicePanelView: UIView {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dsnLabel: UILabel!
     @IBOutlet weak var connectivityLabel: UILabel!
+    @IBOutlet weak var oemModelLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var macAddressLabel: UILabel!
+    @IBOutlet weak var ipAddressLabel: UILabel!
+    
     
     func configure(device:AylaDevice) {
-        
-        nameLabel.text = device.productName
-        dsnLabel.text = device.dsn
+        nameLabel.text = String(format: "%@", device.productName!)
+        dsnLabel.text = String(format: "%@ %@", "DSN: ", device.dsn!)
         connectivityLabel.text = device.connectionStatus
+        oemModelLabel.text = String(format: "%@ %@", "OEM Model: ", device.oemModel!)
+        modelLabel.text = String(format: "%@ %@", "Model: ", device.model!)
         
+        var macAddress = String()
+        if let mac = device.mac {
+            macAddress = mac
+        }
+        else {
+            macAddress = "(null)"
+        }
+        macAddressLabel.text = String(format: "%@ %@", "MAC: ", macAddress)
+        
+        var ipAddress = String()
+        if let mac = device.lanIp {
+            ipAddress = mac
+        }
+        else {
+            ipAddress = "(null)"
+        }
+        ipAddressLabel.text = String(format: "%@ %@", "LAN IP: ", ipAddress)
         self.layer.borderWidth = 0.5
         self.layer.borderColor = UIColor.darkGrayColor().CGColor
+
     }
 }
