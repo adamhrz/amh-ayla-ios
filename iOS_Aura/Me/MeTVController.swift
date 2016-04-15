6573e0e8d3a3cb72a26482a329e53dff0f508c62
Kevin Bella <kevin@aylanetworks.com>
2016-04-15 12:58:01 -0700

    Adds first pass error handling to log out, and forces back out to login screen, even if logout operation fails.

--- iOS_Aura/Me/MeTVController.swift
+++ iOS_Aura/Me/MeTVController.swift
@@ -30,7 +30,23 @@ class MeTVController: UITableViewController {
                 self.navigationController?.tabBarController?.dismissViewControllerAnimated(true, completion: { () -> Void in
                 });
                 }, failure: { (error) -> Void in
-                    
+                    print("Log out operation failed: %@", error)
+                    func alertWithLogout (message: String!, buttonTitle: String!){
+                        let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                        let okAction = UIAlertAction (title: buttonTitle, style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                            
+                            self.navigationController?.tabBarController?.dismissViewControllerAnimated(true, completion: { () -> Void in
+                            });
+                        })
+                        alert.addAction(okAction)
+                        self.presentViewController(alert, animated: true, completion: nil)
+                    }
+                    if error.code == 1002 {
+                        alertWithLogout("Your connection to the internet appears to be offline.  Could not log out properly.", buttonTitle: "Continue")
+                    }
+                    else {
+                        alertWithLogout("An unknown error has occurred", buttonTitle: "Continue")
+                    }
             })
         }
     
